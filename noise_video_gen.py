ed92d5878453b1975588a49511501905fc83bb12
Zhenning Yang <zhenningyang0139@gmail.com>
2021-02-19 16:05:13 -0500

    noisy_video

---
+++ noise_video_gen.py
@@ -0,0 +1,97 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def saltAndPapper_noise(image, s_vs_p=0.5, amount=0.05):
+    row,col,ch = image.shape
+    # s_vs_p = 0.5
+    # amount = 0.05
+    out = np.copy(image)
+    # Salt mode
+    num_salt = np.ceil(amount * image.size * s_vs_p)
+    coords = [np.random.randint(0, i - 1, int(num_salt)) for i in image.shape]
+    out[coords] = 1
+
+    # Pepper mode
+    num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+    coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in image.shape]
+    out[coords] = 0
+    return out
+
+def gauss_noise(image, mean=0, var=0.1):
+    row,col,ch= image.shape
+    # mean = 0
+    # var = 0.1
+    sigma = var**0.5
+    gauss = np.random.normal(mean,sigma,(row,col,ch))
+    gauss = gauss.reshape(row,col,ch)
+    noisy = image + gauss
+    return noisy
+
+def poisson_noise(image):
+    vals = len(np.unique(image))
+    vals = 2 ** np.ceil(np.log2(vals))
+    noisy = np.random.poisson(image * vals) / float(vals)
+    return noisy
+    
+def speckle_noise(image):
+    row,col,ch = image.shape
+    gauss = np.random.randn(row,col,ch)
+    gauss = gauss.reshape(row,col,ch)        
+    noisy = image + image * gauss
+    return noisy
+
+
+if __name__ == '__main__':
+
+    # Create a VideoCapture object
+    # cap = cv2.VideoCapture(0)
+
+    # Check if camera opened successfully
+    #if (cap.isOpened() == False): 
+    #    print("Unable to read camera feed")
+
+    img = cv2.imread('car_detection_sample1.png')
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Default resolutions of the frame are obtained.The default resolutions are system dependent.
+    # We convert the resolutions from float to integer.
+    #frame_width = int(cap.get(3)+10)
+    #frame_height = int(cap.get(4))
+    frame_width = img.shape[1]
+    frame_height = img.shape[0]
+
+    # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file. 
+    # out = cv2.VideoWriter('out.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 15, (frame_width, frame_height))
+    out = cv2.VideoWriter('out.mp4',cv2.VideoWriter_fourcc(*'MP4V'), 15, (frame_width, frame_height))
+
+    org_img = img.copy()
+    amount = 0.05
+    while(True):
+        #ret, frame = cap.read()
+        frame = saltAndPapper_noise(org_img, s_vs_p=0.5, amount=amount)
+        amount+=0.01
+
+        #if ret == True: 
+
+        # Write the frame into the file 'output.avi'
+        out.write(frame)
+
+        # Display the resulting frame    
+        cv2.imshow('frame',frame)
+
+        # Press Q on keyboard to stop recording
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        # Break the loop
+        #else:
+        #    break  
+
+    # When everything done, release the video capture and video write objects
+    # cap.release()
+    out.release()
+
+    # Closes all the frames
+    cv2.destroyAllWindows()
