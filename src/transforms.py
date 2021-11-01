765b16d723a38d5ad8105c6483d8d9373d5f705d
Rus Refati <rrusitan@vols.utk.edu>
2021-11-01 15:15:20 -0400

    a faster implementation of salt and pepper noise #11

--- src/transforms.py
+++ src/transforms.py
@@ -124,18 +124,26 @@ def normal_comp(image, scale_factor):
     return resized_img
 
 def saltAndPapper_noise(image, prob=0.01):
-    output = np.zeros(image.shape,np.uint8)
-    thres = 1 - prob 
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            rdn = random.random()
-            if rdn < prob:
-                output[i][j] = 0
-            elif rdn > thres:
-                output[i][j] = 255
-            else:
-                output[i][j] = image[i][j]
-    return output
+    '''
+    Add salt and pepper noise to image
+    prob: Probability of the noise
+    '''
+    #image = image.copy()
+    if len(image.shape) == 2:
+        black = 0
+        white = 255            
+    else:
+        colorspace = image.shape[2]
+        if colorspace == 3:  # RGB
+            black = np.array([0, 0, 0], dtype='uint8')
+            white = np.array([255, 255, 255], dtype='uint8')
+        else:  # RGBA
+            black = np.array([0, 0, 0, 255], dtype='uint8')
+            white = np.array([255, 255, 255, 255], dtype='uint8')
+    probs = np.random.random(image.shape[:2])
+    image[probs < (prob / 2)] = black
+    image[probs > 1 - (prob / 2)] = white
+    return image
 
 def poisson_noise(image):
     vals = len(np.unique(image))
